9b8019c86d039480c3a07af9a200fb7575fece5a
FinnbarrOC <lukasocallahan@gmail.com>
2019-04-12 07:57:53 -0400

    Finished Python intro code

--- High School Workshops/May2019.py
+++ High School Workshops/May2019.py
@@ -8,10 +8,10 @@ print("Hello, World!")
 
 # 2) VARIABLES: we can use variables like sticky notes to hold data for us
 
-message = "My favorite color is green"
+message = "My favorite color is green"  # str
 print(message)
-
-# TODO: talk about different types, e.g. int and str
+number = 42   # int
+print("My favorite number is " + str(number)) # Converting types
 
 # 3) INPUT: now let's type a message to the computer, save it, and print it back out
 
@@ -37,9 +37,44 @@ else:
 answer = input("Say my name, and I disappear. What am I? ")
 if (answer == "silence"):
   print("You solved my riddle!")
+elif (answer == "Silence"):     # "elif" short for "else if"
+  print("You solved my riddle!")
 else:
   print("Sorry, wrong answer.")
 
 # 6) CALCULATOR: time to combine everything you've learned to make a simple calculator!
 
-# TODO
+number1 = int(input("What is the first number? "))
+number2 = int(input("What is the second number? "))
+operation = input("Would you like to add or subtract? ")
+
+if (operation == "add"):
+  print(number1 + number2)
+else:
+  print(number1 - number2)
+
+# 7) WHILE LOOPS: this is how we can make some lines of code run multiple times, or forever
+
+while(True):
+  number1 = int(input("What is the first number? "))
+  number2 = int(input("What is the second number? "))
+  operation = input("Would you like to add, subtract, or quit? ")
+
+  if (operation == "quit"):
+    print("Goodbye")
+    break
+  elif (operation == "add"):
+    print(number1 + number2)
+  else:
+    print(number1 - number2)
+
+# 8) FOR LOOPS: this is another way to run code multiple times; this should get interesting
+  # (you can learn about factorials here: https://en.wikipedia.org/wiki/Factorial)
+
+number = int(input("What number should we find the factorial of? "))
+total = 1
+for x in range(1, number + 1):  # Check for off-by-one errors here
+  total = total * x
+
+print("The factorial of " + str(number) + " =")
+print(total)
