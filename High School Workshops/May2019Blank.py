519b3feacc2eb33114d24188730b50a8b4db6843
Finnbarr O'Callahan <FinnbarrOC@users.noreply.github.com>
2019-04-30 17:38:31 -0400

    Added blank starter code for students

---
+++ High School Workshops/May2019Blank.py
@@ -0,0 +1,34 @@
+# A line with a hashtag is a "comment"; it's greyed-out and does nothing.
+# We'll use them to leave you notes and instructions below!
+
+
+# 1) OUTPUT: first, we want the computer to show us some text
+
+
+
+# 2) VARIABLES: we can use variables like sticky notes to hold data for us
+
+
+
+# 3) INPUT: now let's type a message to the computer, save it, and print it back out
+
+
+
+# 4) MATH: try some simple addition/subtraction, then see how fast your computer really is!
+
+
+
+# 5) IF/ELSE: this is how your computer makes simple decisions based on "True" or "False"
+
+
+
+# 6) CALCULATOR: time to combine everything you've learned to make a simple calculator!
+
+
+
+# 7) WHILE LOOPS: this is how we can make some lines of code run multiple times, or forever
+
+
+
+# 8) FOR LOOPS: this is another way to run code multiple times; this should get interesting
+  # (you can learn about factorials here: https://en.wikipedia.org/wiki/Factorial)
